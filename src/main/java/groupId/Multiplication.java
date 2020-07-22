c98afd1661797ed6b5494f084f90613ddca90bd9
Marvin <Marvin111Marvin@outlook.com>
2020-07-21 20:15:03 -0400

    Added score after each reset in Squares()

--- src/main/java/groupId/Multiplication.java
+++ src/main/java/groupId/Multiplication.java
@@ -11,17 +11,22 @@ public class Multiplication {
 
     int[] table = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
     boolean[] answered = new boolean[table.length];
+    int previous = 0;
     int square = 0;
+    int missed = 0;
     
     System.out.println("Squares 2-15 (-1 = Cancel)");
 
-    square = getNextSquare(table, answered);
+    square = getNextSquare(table, answered, missed);
     while (answer != -1) {
       System.out.print("  " + square + " * " + square + " = ");
       answer = kb.nextInt();
 
       if (answer == square * square) {
-        square = getNextSquare(table, answered);
+        square = getNextSquare(table, answered, missed);
+      } else if (square != previous) {
+        previous = square;
+        missed++;
       }
     }
 
@@ -29,9 +34,10 @@ public class Multiplication {
     
   }
   
-  public static int getNextSquare(int[] table, boolean[] answered) {
+  public static int getNextSquare(int[] table, boolean[] answered, int missed) {
     boolean remaining = false;
     int index = 0;
+    int correct = 0;
 
     for (int i = 0; i < answered.length; i++) {
       if (answered[i] == false) {
@@ -44,6 +50,9 @@ public class Multiplication {
       for (int i = 0; i < answered.length; i++) {
         answered[i] = false;
       }
+      correct = table.length - missed;
+      System.out.printf("Score: %d/%d = %.2f%% \n", correct, table.length, (double) correct / (double) table.length * 100.0);
+      missed = 0;
       System.out.println("  Reset\n");
     }
 
